b8b41a481984c782c366bce874e4e77f5f637b19
Arturo Villalta <alvillalt@gmail.com>
2025-05-20 15:23:46 +0000

    Termino proyecto

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,55 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+//  React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PropTypes from "prop-types";
 
-//Bootstrap
+//  Selección HTML
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//  Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "bootstrap";
 
-// index.css'
-import '../styles/index.css'
+//  Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 
-// components
-import Home from './components/Home';
+//  index.css'
+import '../styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+function SimpleCounter(props) {
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
+        <FontAwesomeIcon icon={faClock} />
+      </div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
+    </div>
+  );
+}
+
+SimpleCounter.propTypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number
+};
+
+let counter = 0;
+
+setInterval(function() {
+  const four = Math.floor(counter / 1000);
+  const three = Math.floor(counter / 100);
+  const two = Math.floor(counter / 10);
+  const one = Math.floor(counter / 1);
+  counter++;
+  root.render(
+    <SimpleCounter digitFour={four} digitThree={three} digitTwo={two} digitOne={one} />
+    );
+  }, 1000);
+
+ReactDOM.createRoot(root).render(<SimpleCounter />);
